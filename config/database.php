eee8c8c8e80175f9ec7658a29bc6b46e517c03fa
Bagus Putra Mahendra <99731198+bagusmahendra85@users.noreply.github.com>
2024-02-07 00:08:07 +0800

    database credential template

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php 
+
+// database credentials
+define('DATABASE_HOST', 'localhost'); //your database host here
+define('DATABASE_USERNAME', 'root'); // your database username here
+define('DATABASE_PASSWORD', ''); // your database password here
+define('DATABASE_NAME', 'phplearn'); // your database name
+
+
+?>
